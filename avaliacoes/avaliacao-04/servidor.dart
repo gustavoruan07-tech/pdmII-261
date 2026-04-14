2812d3f4f3dfdb2ac4656ecc5a04a5cdd80030fc
gustavoruan07-tech <gustavo.ruan07@aluno.ifce.edu.br>
2026-04-14 16:28:28 -0300

    Update and rename server.dart to servidor.dart

--- avaliacoes/avaliacao-04/servidor.dart
+++ avaliacoes/avaliacao-04/servidor.dart
@@ -2,15 +2,16 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() async {
-  var server = await ServerSocket.bind(InternetAddress.loopbackIPv4, 3000);
-
+  // Criar servidor na porta 3000
+  var server = await ServerSocket.bind('127.0.0.1', 3000);
   print('Servidor rodando na porta 3000...');
 
+  // Esperar conexões
   await for (var socket in server) {
     print('Cliente conectado!');
 
     socket.listen((data) {
-      String mensagem = utf8.decode(data);
+      var mensagem = utf8.decode(data);
       print('Temperatura recebida: $mensagem');
     });
   }
